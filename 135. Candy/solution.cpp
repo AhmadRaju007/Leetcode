07e2d58b0b5d306d057cf3c46d5d120c8a1e278d
AhmadRaju007 <rajuahmad6789@gmail.com>
2022-07-04 23:51:08 +0600

    greedy hard O(n) Solution

---
+++ 135. Candy/solution.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int sum=0, n= ratings.size();
+        vector<int>v(n,1);
+        
+        for(int i=1; i<n; ++i){
+            if(ratings[i]>ratings[i-1]){
+                v[i]=v[i-1]+1;
+            }
+        }
+        
+        for(int i=n-2; i>=0; --i){
+            if(ratings[i]>ratings[i+1] ){
+                v[i]= max(v[i], v[i+1]+1);
+            }
+        }
+        
+        for(int i=0; i<n; ++i){
+            sum+= v[i];
+        }
+        
+        return sum;
+    }
+};
